e8b4337738023c1b687e927a2381da07c857d9a3
Daan <Daan@DESKTOP-9K8M9H0>
2019-10-10 15:22:23 +0200

    add repository pattern with domain repos

--- projecten3-1920-backend-klim03/Data/Repos/BestellingRepo.cs
+++ projecten3-1920-backend-klim03/Data/Repos/BestellingRepo.cs
@@ -1,11 +1,47 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using projecten3_1920_backend_klim03.Domain.Models.Domain;
+using projecten3_1920_backend_klim03.Domain.Models.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace projecten3_1920_backend_klim03.Data.Repos
 {
-    public class BestellingRepo
+    public class BestellingRepo : IBestellingRepo
     {
+        private readonly ApplicationDbContext _context;
+        private readonly DbSet<Bestelling> _bestellingen;
+
+        public BestellingRepo(ApplicationDbContext dbContext)
+        {
+            _context = dbContext;
+            _bestellingen = dbContext.Bestellingen;
+        }
+
+        public void Add(Bestelling obj)
+        {
+            _bestellingen.Add(obj);
+        }
+
+        public ICollection<Bestelling> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Bestelling GetById(long id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(Bestelling obj)
+        {
+            _bestellingen.Remove(obj);
+        }
+
+        public void SaveChanges()
+        {
+            _context.SaveChanges();
+        }
     }
 }
--- projecten3-1920-backend-klim03/Data/Repos/KlasRepo.cs
+++ projecten3-1920-backend-klim03/Data/Repos/KlasRepo.cs
@@ -1,11 +1,47 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using projecten3_1920_backend_klim03.Domain.Models.Domain;
+using projecten3_1920_backend_klim03.Domain.Models.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace projecten3_1920_backend_klim03.Data.Repos
 {
-    public class KlasRepo
+    public class KlasRepo : IKlasRepo
     {
+        private readonly ApplicationDbContext _context;
+        private readonly DbSet<Klas> _klassen;
+
+        public KlasRepo(ApplicationDbContext dbContext)
+        {
+            _context = dbContext;
+            _klassen = dbContext.Klassen;
+        }
+
+        public void Add(Klas obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ICollection<Klas> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Klas GetById(long id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(Klas obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SaveChanges()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
