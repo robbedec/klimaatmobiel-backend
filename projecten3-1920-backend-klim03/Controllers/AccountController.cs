402457ff6716308e517747863b0e5b456a19528d
robbedec <robbe.decorte@student.hogent.be>
2019-10-11 17:51:12 +0200

    feat: Acount endpoints

---
+++ projecten3-1920-backend-klim03/Controllers/AccountController.cs
@@ -0,0 +1,136 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using projecten3_1920_backend_klim03.Domain.Models.Domain;
+using projecten3_1920_backend_klim03.Domain.Models.DTOs;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace projecten3_1920_backend_klim03.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [ApiConventionType(typeof(DefaultApiConventions))]
+    public class AccountController : ControllerBase
+    {
+        private readonly SignInManager<Gebruiker> _signInManager;
+        private readonly UserManager<Gebruiker> _userManager;
+        private readonly IConfiguration _config;
+
+        public AccountController(SignInManager<Gebruiker> signInManager, UserManager<Gebruiker> userManager, IConfiguration config)
+        {
+            _signInManager = signInManager;
+            _userManager = userManager;
+            _config = config;
+        }
+
+        /// <summary>
+        /// Login
+        /// </summary>
+        /// <param name="model">the login details</param>
+        [AllowAnonymous]
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<ActionResult<String>> CreateToken(LoginDTO model)
+        {
+            Gebruiker user;
+            if (model.UserName.IndexOf('@') > -1)
+            {
+                //Validate email format
+                string emailRegex = @"^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}" +
+                                       @"\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\" +
+                                          @".)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$";
+                Regex re = new Regex(emailRegex);
+
+                if (!re.IsMatch(model.UserName))
+                {
+                    return BadRequest();
+                }
+                user = await _userManager.FindByEmailAsync(model.UserName);
+            }
+            else
+            {
+                user = await _userManager.FindByNameAsync(model.UserName);
+            }
+
+            if (user != null)
+            {
+                var result = await _signInManager.CheckPasswordSignInAsync(user, model.Password, false);
+
+                if (result.Succeeded)
+                {
+                    string token = GetToken(user);
+                    return Created("", token); //returns only the token                    
+                }
+            }
+            return BadRequest();
+        }
+
+        /// <summary>
+        /// Register a user
+        /// </summary>
+        /// <param name="model">the user details</param>
+        /// <returns></returns>
+        [AllowAnonymous]
+        [HttpPost("register")]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<ActionResult<String>> Register(RegisterDTO model)
+        {
+            Gebruiker user = new Gebruiker { UserName = model.UserName, Email = model.Email, Voornaam = model.FirstName, Achternaam = model.LastName };
+            var result = await _userManager.CreateAsync(user, model.Password);
+
+            if (result.Succeeded)
+            {
+                string token = GetToken(user);
+                return Created("", token);
+            }
+            return BadRequest();
+        }
+
+        /// <summary>
+        /// Checks if an email is available as username
+        /// </summary>
+        /// <returns>true if the email is not registered yet</returns>
+        /// <param name="email">Email.</param>/
+        [AllowAnonymous]
+        [HttpGet("checkusername")]
+        public async Task<ActionResult<Boolean>> CheckAvailableUserName(string email)
+        {
+            var user = await _userManager.FindByNameAsync(email);
+            return user == null;
+        }
+
+        private string GetToken(Gebruiker user)
+        {
+            // Create the token
+            var claims = new[]
+            {
+              new Claim(JwtRegisteredClaimNames.Sub, user.Email),
+              new Claim(JwtRegisteredClaimNames.UniqueName, user.UserName)
+            };
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["Tokens:Key"]));
+
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            var token = new JwtSecurityToken(
+              null, null,
+              claims,
+              expires: DateTime.Now.AddMinutes(30),
+              signingCredentials: creds);
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+    }
+}
---
+++ projecten3-1920-backend-klim03/Domain/Models/DTOs/LoginDTO.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace projecten3_1920_backend_klim03.Domain.Models.DTOs
+{
+    public class LoginDTO
+    {
+        // https://techbrij.com/asp-net-core-identity-login-email-username
+
+
+        /// <example>
+        /// "robbedec"
+        /// </example>
+        [Required]
+        public string UserName { get; set; }
+
+        /// <example>
+        /// "P@ssword1"
+        /// </example>
+        [Required]
+        public string Password { get; set; }
+
+    }
+}
