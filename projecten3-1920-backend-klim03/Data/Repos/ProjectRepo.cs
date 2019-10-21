9c6fc9fdaed6495cde894d79ce5f553fdd9f37ad
Daan <Daan@DESKTOP-9K8M9H0>
2019-10-21 17:20:35 +0200

    fix: nullerror order when fetching project by id

--- projecten3-1920-backend-klim03/Data/Repos/ProjectRepo.cs
+++ projecten3-1920-backend-klim03/Data/Repos/ProjectRepo.cs
@@ -36,7 +36,7 @@ namespace projecten3_1920_backend_klim03.Data.Repos
             return _projects
                 .Include(g => g.ClassRoom)
                 .Include(g => g.Products).ThenInclude(g => g.Category)
-                .Include(g => g.Groups)
+                .Include(g => g.Groups).ThenInclude(g => g.Order)
                 .Include(g => g.ApplicationDomain)
                 .SingleOrDefault(g => g.ProjectId == id);
         }
