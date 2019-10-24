8586285445eec31adce2297d514e0c4e4de36f5d
robbedec <robbe.decorte@student.hogent.be>
2019-10-24 09:41:11 +0200

    chore: testing project

---
+++ projecten3-1920-backend-klim03.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace projecten3_1920_backend_klim03.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
