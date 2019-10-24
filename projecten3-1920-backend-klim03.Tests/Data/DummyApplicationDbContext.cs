7583b6956f1fcef85b172efd3a27e7afce0217c2
robbedec <robbe.decorte@student.hogent.be>
2019-10-24 10:47:09 +0200

    feat: order testing

---
+++ projecten3-1920-backend-klim03.Tests/Data/DummyApplicationDbContext.cs
@@ -0,0 +1,81 @@
+﻿using projecten3_1920_backend_klim03.Domain.Models.Domain;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace projecten3_1920_backend_klim03.Tests.Data
+{
+    public class DummyApplicationDbContext
+    {
+        public Order testOrder { get; }
+        public OrderItem testOrderItem { get; }
+
+        public DummyApplicationDbContext()
+        {
+            Product testP1 = new Product
+            {
+                ProductId = 1,
+                ProductName = "Karton",
+                Description = "Karton beschrijving",
+                Price = 5
+            };
+
+            Product testP2 = new Product
+            {
+                ProductId = 2,
+                ProductName = "Plastiek",
+                Description = "plastiek beschrijving",
+                Price = 20
+            };
+
+            Product testP3 = new Product
+            {
+                ProductId = 3,
+                ProductName = "Lijm",
+                Description = "Lijm beschrijving",
+                Price = 8.5
+            };
+
+            OrderItem testOrderItem1 = new OrderItem
+            {
+                OrderItemId = 1,
+                Product = testP1,
+                Amount = 2
+            };
+
+            OrderItem testOrderItem2 = new OrderItem
+            {
+                OrderItemId = 2,
+                Product = testP2,
+                Amount = 1
+            };
+
+            OrderItem testOrderItem3 = new OrderItem
+            {
+                OrderItemId = 3,
+                Product = testP3,
+                Amount = 3
+            };
+
+            testOrderItem = new OrderItem
+            {
+                OrderItemId = 4,
+                Product = testP1,
+                Amount = 5
+            };
+
+            testOrder = new Order
+            {
+                OrderId = 1,
+                Approved = false,
+                Submitted = false,
+                OrderItems = new List<OrderItem>
+                {
+                    testOrderItem1,
+                    testOrderItem2,
+                    testOrderItem3
+                }
+            };
+        }
+    }
+}
---
+++ projecten3-1920-backend-klim03.Tests/Models/OrderTest.cs
@@ -0,0 +1,85 @@
+﻿using projecten3_1920_backend_klim03.Domain.Models.Domain;
+using projecten3_1920_backend_klim03.Domain.Models.CustomExceptions;
+using projecten3_1920_backend_klim03.Tests.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit;
+
+namespace projecten3_1920_backend_klim03.Tests.Models
+{
+    public class OrderTest : IDisposable
+    {
+        private readonly DummyApplicationDbContext _dummyApplicationDbContext;
+        private Order testOrder;
+
+        public OrderTest()
+        {
+            _dummyApplicationDbContext = new DummyApplicationDbContext();
+            testOrder = _dummyApplicationDbContext.testOrder;
+        }
+
+        [Fact]
+        public void Order_AddItem_AddsItem()
+        {
+            int amountBefore = testOrder.OrderItems.Count;
+
+            testOrder.AddOrderItem(_dummyApplicationDbContext.testOrderItem);
+
+            Assert.Equal(amountBefore + 1, testOrder.OrderItems.Count);
+        }
+
+        [Fact]
+        public void Order_AddItemToSubmittedOrder_ThrowsError()
+        {
+            testOrder.Submitted = true;
+            Assert.Throws<NotSupportedException>(() => testOrder.AddOrderItem(_dummyApplicationDbContext.testOrderItem));
+        }
+
+        [Fact]
+        public void Order_RemoveItem_RemovesItem()
+        {
+            int amountBefore = testOrder.OrderItems.Count;
+
+            testOrder.RemoveOrderItem(testOrder.OrderItems.First());
+
+            Assert.Equal(amountBefore - 1, testOrder.OrderItems.Count);
+        }
+
+        [Fact]
+        public void Order_RemoveItemToSubmittedOrder_ThrowsError()
+        {
+            testOrder.Submitted = true;
+            Assert.Throws<NotSupportedException>(() => testOrder.RemoveOrderItem(testOrder.OrderItems.First()));
+        }
+
+        [Fact]
+        public void Order_GetOrderItemById_CorrectItem()
+        {
+            OrderItem it = testOrder.GetOrderItemById(1);
+
+            Assert.NotNull(it);
+            Assert.Equal("Karton", it.Product.ProductName);
+            Assert.Equal(2, it.Amount);
+            Assert.Equal(5, it.Product.Price);
+        }
+
+        [Fact]
+        public void Order_GetOrderItemByNonExistantId_ThrowsError()
+        {
+            Assert.Throws<DomainArgumentNullException>(() => testOrder.GetOrderItemById(100));
+        }
+
+        [Fact]
+        public void Order_CheckOrderPrice_CorrectPrice()
+        {
+            Assert.Equal(Convert.ToDecimal(55.5), testOrder.GetOrderPrice, 2);
+        }
+
+        public void Dispose()
+        {
+            testOrder = _dummyApplicationDbContext.testOrder;
+        }
+    }
+}
